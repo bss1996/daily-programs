e748cfe0aaa26d8fd13247e7b2c7ffdd72834c91
Bhawani singh <shekhawat.bs13@gmail.com>
2023-09-07 09:52:24 +0530

    teacherday

--- teachers_day.py
+++ teachers_day.py
@@ -1,6 +1,6 @@
 print('\n'.join
       ([''.join
-        ([('Teachers'[(x-y)%8 ]
+        ([('Teachersday'[(x-y)%11]
            if((x*0.05)**2+(y*0.1)**2-1)
            **3-(x*0.05)**2*(y*0.1)
            **3<=0 else' ')
